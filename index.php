287bf3fc138812c4603b790ba370f37346399984
garc2155 <juangarcia@csumb.edu>
2016-09-21 20:45:27 +0000

    adding functions to simplify process

--- index.php
+++ index.php
@@ -4,6 +4,29 @@
         <title> </title>
     </head>
     <body>
-    <!-- adding a message for commit --> 
+        
+    <?php
+        // Make card
+        function displayCard($cardLocation){
+        echo "<img class='card' src='" . $cardLocation . "'>";
+        }
+        
+        function displayHand($cards){
+            foreach ($cards as $card) {
+                displayCard($card);
+            }
+        }
+        
+        
+        displayCard("img/card_back.png");
+        //displayCard("img/clubs/1.png");
+        
+        $cards = array("clubs","diamonds","hearts","spades");
+        
+        
+        // make deck
+        // make hands
+        // show hands and users
+    ?>
     </body>
 </html>
