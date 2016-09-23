50cf4e66513da990a93201e25782f8a8c7ed7b7b
Michael <micfernandez@csumb.edu>
2016-09-23 17:02:25 +0000

    Added shuffle, rand hand sizes, starting to add loops

--- index.php
+++ index.php
@@ -25,59 +25,70 @@
 
 <?php
 $cardsArrayIndex = [
-    "Ace" => "Hearts",
-    "Two" => "Hearts", 
-    "Three" => "Hearts", 
-    "Four" => "Hearts", 
-    "Five" => "Hearts", 
-    "Six" => "Hearts", 
-    "Seven" => "Hearts",
-    "Eight" => "Hearts",
-    "Nine" => "Hearts",
-    "Jack" => "Hearts",
-    "Queen" => "Hearts",
-    "King" => "Hearts",
-    
-    "Ace" => "Diamonds",
-    "Two" => "Diamonds", 
-    "Three" => "Diamonds", 
-    "Four" => "Diamonds", 
-    "Five" => "Diamonds", 
-    "Six" => "Diamonds", 
-    "Seven" => "Diamonds",
-    "Eight" => "Diamonds",
-    "Nine" => "Diamonds",
-    "Jack" => "Diamonds",
-    "Queen" => "Diamonds",
-    "King" => "Diamonds",
-    
-    "Ace" => "Clubs",
-    "Two" => "Clubs", 
-    "Three" => "Clubs", 
-    "Four" => "Clubs", 
-    "Five" => "Clubs", 
-    "Six" => "Clubs", 
-    "Seven" => "Clubs",
-    "Eight" => "Clubs",
-    "Nine" => "Clubs",
-    "Jack" => "Clubs",
-    "Queen" => "Clubs",
-    "King" => "Clubs",
-    
-    "Ace" => "Spades",
-    "Two" => "Spades", 
-    "Three" => "Spades", 
-    "Four" => "Spades", 
-    "Five" => "Spades", 
-    "Six" => "Spades", 
-    "Seven" => "Spades",
-    "Eight" => "Spades",
-    "Nine" => "Spades",
-    "Jack" => "Spades",
-    "Queen" => "Spades",
-    "King" => "Spades"
+    "Ace of Hearts",
+    "Two of Hearts", 
+    "Three of Hearts", 
+    "Four of Hearts", 
+    "Five of Hearts", 
+    "Six of Hearts", 
+    "Seven of Hearts",
+    "Eight of Hearts",
+    "Nine of Hearts",
+    "Jack of Hearts",
+    "Queen of Hearts",
+    "King of Hearts",
+    "Ace of Diamonds",
+    "Two of Diamonds", 
+    "Three of Diamonds", 
+    "Four of Diamonds", 
+    "Five of Diamonds", 
+    "Six of Diamonds", 
+    "Seven of Diamonds",
+    "Eight of Diamonds",
+    "Nine of Diamonds",
+    "Jack of Diamonds",
+    "Queen of Diamonds",
+    "King of Diamonds",
+    "Ace of Clubs",
+    "Two of Clubs", 
+    "Three of Clubs", 
+    "Four of Clubs", 
+    "Five of Clubs", 
+    "Six of Clubs", 
+    "Seven of Clubs",
+    "Eight of Clubs",
+    "Nine of Clubs",
+    "Jack of Clubs",
+    "Queen of Clubs",
+    "King of Clubs",
+    "Ace of Spades",
+    "Two of Spades", 
+    "Three of Spades", 
+    "Four of Spades", 
+    "Five of Spades", 
+    "Six of Spades", 
+    "Seven of Spades",
+    "Eight of Spades",
+    "Nine of Spades",
+    "Jack of Spades",
+    "Queen of Spades",
+    "King of Spades"
     ];
-
+    
+    
+    shuffle ($cardsArrayIndex);
+    echo "Your cards have been shuffled<br />";
+    
+    $numCards1 = rand(4, 6);
+    $numCards2 = rand(4, 6);
+    $numCards3 = rand(4, 6);
+    $numCards4 = rand(4, 6);
+    
+    $player1Hand[numCards1];
+    
+    foreach($cardsArrayIndex as $card){
+        echo $card."<br />";
+    }
 ?>
     </body>
 </html>
